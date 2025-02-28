00cee2f35162d3f6df735b98374ff4a0740fc024
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-02-28 12:06:48 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+// how to declare constant and variables in js 
+
+const accId =1234
+let accemail ="manav@gm.com"
+
+// we dont use var in js because of issue in block scope and function scope
+
+var accpass ="1212"
+
+//if you dont give any value to the variable then it will show you undefined
+let accname
+
+
+accemail="harsh@gm.com"
+accpass="4545"
+
+//we use a table here to show all fields in one time 
+
+console.table([accId,accemail,accpass,accname])
