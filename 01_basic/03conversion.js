147e6db0412225332e482e43ea5cd680061ae934
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-03-01 19:04:17 +0000

    conversion

--- 01_basic/03conversion.js
+++ 01_basic/03conversion.js
@@ -22,9 +22,35 @@
  // ""-> false
  // "manav" -> true
 
-let somenumber= 33
+// let somenumber= 33
 
-let value2 = String(somenumber)
-console.log(value2);
-console.log(typeof value2);
+// let value2 = String(somenumber)
+// console.log(value2);
+// console.log(typeof value2);
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2 is multiplied 3 times
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "manav"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
