7dc75473c1466a796a9ae5633d0ff2b41701876b
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-03-01 17:25:23 +0000

    datatypes

---
+++ 01_basic/02datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as a newer version
+
+// alert( 3+3) // We are using node js, not browser
+
+console.log(3+3);
+console.log("manav");//code readablity should be high
+
+let name = "manav"
+let age = 21
+let value = true
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique // use in mostly react to provide a unique value
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); //output will be object
+console.log(typeof undefined); // output will be undefined
+
