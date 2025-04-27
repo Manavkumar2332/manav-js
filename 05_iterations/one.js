fa0babe59e9d5b08d2e94c5cd7f8e26e6effbf35
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-04-27 10:06:16 +0000

    iteration

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -32,14 +32,31 @@
 
 
 
-let array1 = [ "mk","hk", "yk" ]
+// let array1 = [ "mk","hk", "yk" ]
 
-for (let index = 0; index < array1.length; index++) {
-    const element = array1[index];
-    console.log(element);
+// for (let index = 0; index < array1.length; index++) {
+//     const element = array1[index];
+//     console.log(element);
+    
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5)
+    {
+        console.log(`detected 5`);
+        
+        continue
+    }
+
+    console.log(`value of i is ${index}`);
     
     
 }
+// continue helps you to skip that particualr iteration like in this example 5 is skiped 
+
+// break helps tp break the loop after you got you result and can directly use in switch but continue is use indirectly, use in especially loop
+
 
 
 
