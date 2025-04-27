a24c6f7525de3fb11cd81f715fb19e2c17c71f8d
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-04-27 09:59:05 +0000

    iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+// // for loop
+
+// for(let i =0 ; i<=10 ; i++)
+// {
+//    const element = i;
+//    if(element== 5)
+//    {
+//     // console.log("5 is the best number");
+    
+//    }
+//     // console.log(element);
+    
+// }
+
+// for(let i =1 ; i<=10 ; i++)
+// {
+//     // console.log(`outer loop ${i}`)
+//     for(let j=1 ; j<=10 ; j++)
+//     {
+//     //   console.log( i + '*' + j + '=' + i*j);
+      
+//     }// use to print tables
+// }
+
+// // while loop
+// // let i=0
+// // while(i<10)
+// // {
+// //     // console.log(i);
+// //     // i++;
+// // }
+
+
+
+let array1 = [ "mk","hk", "yk" ]
+
+for (let index = 0; index < array1.length; index++) {
+    const element = array1[index];
+    console.log(element);
+    
+    
+}
+
+
+
