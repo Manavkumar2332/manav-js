30581471d7a542f83b247efe24eb7ff619724654
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-06-10 09:53:46 +0000

    asyn_code

---
+++ 08_event/async_code.js
@@ -0,0 +1,28 @@
+//Asynchronus code
+
+// default java script{
+// javascript is synchronus - code will be execute one by one
+
+// javascript is single threaded language- everything execute on a single thread }
+
+/*execution context
+execute one line of code at a time
+console.log1
+console.log2 - each operation waits for the last one to complete before executing
+CALL STACK - a stack is created and the code is executed into it which follows lifo rule 
+MEMORY HEAP 
+*/
+
+/*
+two types of code is written in js
+ 
+1.BLOCKING CODE- block the flow of program
+
+2. NON BLOCKING CODE - does not block execution
+
+*/
+
+/**
+ * In synchronus js the code is executed on js single thread 
+ * In asynchrnus js the code is executed by using api and then api are called by register call back to tell at which event you have to give response then it goes to task queue and then to call stack again
+ */
