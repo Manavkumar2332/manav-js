d3c544a42ea504792d93f401eb77826a21e2c305
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2025-06-12 20:03:01 +0000

    objects

---
+++ 10_classes_oop/object.js
@@ -0,0 +1,36 @@
+function multipyby5 (num){
+    return num*5
+}
+multipyby5.power=2
+// function is function as well as object
+
+ console.log(multipyby5(5));
+ console.log(multipyby5.power); // we added property by using dot to show that a function is a object as uk object elements can be access by dot only
+ console.log(multipyby5.prototype);//Every function gets a prototype object by default
+
+
+ //CONSTRUTOR FUNCTION
+ function createuser(username,score){
+    this.username = username
+    this.score= score
+ }
+
+ createuser.prototype.increment = function(){
+    this.score++
+ }
+createuser.prototype.printme = function(){
+    console.log(`score is ${this.score}`)
+}
+ const chai = new createuser("chai",25)
+ const tea = new createuser("tea",20)
+
+ chai.printme()
+ tea.printme()
+/**
+ * ✅ First new creates an empty object,
+✅ then this refers to that object,
+✅ and finally that object is returned automatically.
+ */
+ 
+ 
+ 
---
+++ 10_classes_oop/prototype.js
@@ -0,0 +1,5 @@
+// js is prototype in nature
+// array is object in js
+// object has no parent it refers to null
+// string also comes from object
+ 
